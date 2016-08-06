23804a6dbda2dcd86d8b554a2accb6d2a09464de
CalebOuellette <caleb.ouellette@gmail.com>
2016-08-06 16:52:06 -0700

    start of paste impelentation.

--- src/extension.ts
+++ src/extension.ts
@@ -3,21 +3,33 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {Copy} from './copy';
+import { Paste } from './paste';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     var copyHolder = new Copy;
 
+    var paste = new Paste;
+
     let keys = 9;
     let copyCommands = [];
+    let pasteCommands = [];
     for (let index = 0; index < keys; index++) { //build the 10 copy commands. Can't pass data into commands to this seems like the best way?
-        let name = 'copy ' + index;
-        let cmd = vscode.commands.registerCommand(name, () => {                    
-            copyHolder.saveHighlight(name);            
-            vscode.window.showInformationMessage('Hello ' + copyHolder.getItem(name));
+        let copyName = 'copy ' + index;
+        let copyCmd = vscode.commands.registerCommand(copyName, () => {                    
+            copyHolder.saveHighlight(copyName);            
+            vscode.window.showInformationMessage('Hello ' + copyHolder.getItem(copyName));
         })
-        copyCommands.push(cmd);
+        copyCommands.push(copyCmd);
+
+        let pasteName = 'paste ' + index;
+        let pasteCmd = vscode.commands.registerCommand(pasteName, () => {                    
+            paste.pasteText();        
+            vscode.window.showInformationMessage('Hello ' + copyHolder.getItem(pasteName));
+        })
+        pasteCommands.push(copyCmd);
+
     }
 
 
--- src/paste.ts
+++ src/paste.ts
@@ -1,8 +1,66 @@
 import * as vscode from 'vscode';
-
+import {  Position, Range, TextEdit, Uri} from 'vscode';
 export class Paste {
 
     constructor() {
-        vscode.window.activeTextEditor.document
+
     }
+
+    //Build off of this guide: http://www.chrisstead.com/archives/1082/visual-studio-code-extensions-editing-the-document/  
+
+    pasteText() {
+
+       
+        let pos = vscode.window.activeTextEditor.selection.active;
+        this.applyEdit(pos, 'hello');
+
+    }
+
+    applyEdit(coords, content) {
+        var vsDocument = vscode.window.activeTextEditor.document;
+        var edit = this.setEditFactory(vsDocument.uri, coords, content);
+        vscode.workspace.applyEdit(edit);
+
+    }
+
+    positionFactory(line: number, char: number) {
+        return new vscode.Position(line, char);
+    }
+
+
+
+    rangeFactory(start: Position, end: Position) {
+        return new vscode.Range(start, end);
+    }
+
+
+    textEditFactory(range: Range, content: string) {
+        return new vscode.TextEdit(range, content);
+    }
+
+
+
+    editFactory(coords, content) {
+         var start = this.positionFactory(coords.start.line, coords.start.char);
+        var end = this.positionFactory(coords.end.line, coords.end.char);
+        var range = this.rangeFactory(start, end);
+
+        return this.textEditFactory(range, content);
+    }
+
+
+    workspaceEditFactory() {
+        return new vscode.WorkspaceEdit();
+    }
+
+
+    setEditFactory(uri: Uri, coords, content) {
+        var workspaceEdit = this.workspaceEditFactory();
+        var edit = this.editFactory(coords, content);
+
+        workspaceEdit.set(uri, [edit]);
+        return workspaceEdit;
+    }
+
+
 }
